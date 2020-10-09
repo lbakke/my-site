df61e471e5420a2c94decaa8698628643b71f46f
lbakke <lbakke@nd.edu>
2020-10-09 00:49:34 -0400

    Updates

---
+++ precache-manifest.163d55fc0f9c7a5692ed24a5e6e61fe9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972acbb6ece0345d69b21f8cfcb7303c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7ca47bfd2d513711b6b",
+    "url": "/portfolio/static/css/2.b0925b0c.chunk.css"
+  },
+  {
+    "revision": "12c8df2591b06c944d0f",
+    "url": "/portfolio/static/css/main.4ff5f944.chunk.css"
+  },
+  {
+    "revision": "e7ca47bfd2d513711b6b",
+    "url": "/portfolio/static/js/2.d44cb007.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.d44cb007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c8df2591b06c944d0f",
+    "url": "/portfolio/static/js/main.88ea689c.chunk.js"
+  },
+  {
+    "revision": "529e6ab71e8d34dccd1a",
+    "url": "/portfolio/static/js/runtime-main.3e49eb5f.js"
+  },
+  {
+    "revision": "fb72d76f7a08268f01cf374dc6785f54",
+    "url": "/portfolio/static/media/Domotika-Regular-trial.fb72d76f.ttf"
+  },
+  {
+    "revision": "5faa0c05f9464d23657ced77105117a1",
+    "url": "/portfolio/static/media/Gidole-Regular.5faa0c05.otf"
+  },
+  {
+    "revision": "9d1044ccdbba0efa9a2bfc719a446702",
+    "url": "/portfolio/static/media/RobotoMono-Light.9d1044cc.ttf"
+  },
+  {
+    "revision": "7cfbd4284ec01b7ace2f8edb5cddae84",
+    "url": "/portfolio/static/media/RobotoMono-Medium.7cfbd428.ttf"
+  },
+  {
+    "revision": "f0f9269261830c3f3a7ca874c4434512",
+    "url": "/portfolio/static/media/United-lgZww.f0f92692.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
